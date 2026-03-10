1a415746239fcda93c59c071d67225b5bad070a6
Ahmadou Badawi <ahma434@gmail.com>
2026-03-10 02:03:50 +0000

    Update main_plugin.py

--- main_plugin.py
+++ main_plugin.py
@@ -37,7 +37,7 @@ class VerifPlugin:
         self.iface.addPluginToMenu("&Vérif Accès", self.action)
 
     def demander_mot_de_passe(self):
-        MOT_DE_PASSE = "123"
+        MOT_DE_PASSE = "1234"
         mdp, ok = QInputDialog.getText(
             None,
             "Accès restreint",
@@ -62,5 +62,6 @@ class VerifPlugin:
     def unload(self):
         self.iface.removeToolBarIcon(self.action)
         self.iface.removePluginMenu("&Vérif Accès", self.action)
+
 
 
